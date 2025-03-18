48f93052db6b871c1ae6ba87f8e7ad580401a8f3
Sydney Zheng <sydneyzhng@gmail.com>
2025-03-18 00:45:03 -0400

    update

--- 6-RShell/starter/bats/student_tests.sh
+++ 6-RShell/starter/bats/student_tests.sh
@@ -4,16 +4,6 @@
 # 
 # Create your unit tests suit in this file
 
-setup() {
-    ./dsh -s &
-    DSH_PID=$!
-    sleep 1
-}
-
-teardown() {
-   kill $DSH_PID
-}
-
 @test "Example: check ls runs without errors" {
     run ./dsh <<EOF                
 ls
@@ -23,7 +13,7 @@ EOF
     [ "$status" -eq 0 ]
 }
 
-@test "remote test for common command" {
+@test "client server echo test" {
     ./dsh -s -p 12345 &
     server_pid=$!
     sleep 1
@@ -35,6 +25,70 @@ EOF
     kill $server_pid
     stripped_output=$(echo "$output" | tr -d '[:space:]')
     expected_output="socketclientmode:addr:127.0.0.1:12345dsh4>helloworlddsh4>cmdloopreturned0"
+
+    echo "Captured stdout:"
+    echo "Output: $output"
+    echo "Exit Status: $status"
+    echo "${stripped_output} -> ${expected_output}"
+
+    [ "$stripped_output" = "$expected_output" ]
+    [ "$status" -eq 0 ]
+}
+
+@test "client server pipe test" {
+    ./dsh -s -p 12345 &
+    server_pid=$!
+    sleep 1
+
+    run ./dsh -c -i 127.0.0.1 -p 12345 <<EOF
+ls | grep dshlib.c
+EOF
+
+    kill $server_pid
+    stripped_output=$(echo "$output" | tr -d '[:space:]')
+    expected_output="socketclientmode:addr:127.0.0.1:12345dsh4>dshlib.cdsh4>cmdloopreturned0"
+
+    echo "Captured stdout:"
+    echo "Output: $output"
+    echo "Exit Status: $status"
+    echo "${stripped_output} -> ${expected_output}"
+
+    [ "$stripped_output" = "$expected_output" ]
+    [ "$status" -eq 0 ]
+}
+
+@test "exit closes client" {
+    ./dsh -s -p 12345 &
+    server_pid=$!
+    sleep 1
+
+    run ./dsh -c -i 127.0.0.1 -p 12345 <<EOF
+exit
+EOF
+
+    kill $server_pid
+    stripped_output=$(echo "$output" | tr -d '[:space:]')
+    expected_output="socketclientmode:addr:127.0.0.1:12345dsh4>cmdloopreturned0"
+
+    echo "Captured stdout:"
+    echo "Output: $output"
+    echo "Exit Status: $status"
+    echo "${stripped_output} -> ${expected_output}"
+
+    [ "$stripped_output" = "$expected_output" ]
+    [ "$status" -eq 0 ]
+}
+
+@test "stop-server closes client and server" {
+    ./dsh -s -p 12345 &
+    server_pid=$!
+    sleep 1
+
+    run ./dsh -c -i 127.0.0.1 -p 12345 <<EOF
+stop-server
+EOF
+    stripped_output=$(echo "$output" | tr -d '[:space:]')
+    expected_output="socketclientmode:addr:127.0.0.1:12345dsh4>cmdloopreturned0"
 
     echo "Captured stdout:"
     echo "Output: $output"
